6d124d4e5704209c0caef3c91e304fb8d9f28db4
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-05-16 22:22:48 +0530

    .

---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App.js";
+import Home from "../pages/Home.js";
+import ExplorePage from "../pages/ExplorePage.js";
+import DetailsPage from "../pages/DetailsPage.js";
+import SearchPage from "../pages/SearchPage.js";
+
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <App/>,
+        children : [
+            {
+                path : "",
+                element : <Home/>
+            },
+            {
+                path : ":explore",
+                element : <ExplorePage/>
+            },
+            {
+                path : ":explore/:id",
+                element : <DetailsPage/>
+            },
+            {
+                path : "search",
+                element : <SearchPage/>
+            }
+        ]
+    }
+]);
+
+
+export default router;
