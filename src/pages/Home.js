4625235ad7abfe95afdcead91d727637ff4e5921
AnuragShukla1999 <shuklaanurag310@gmail.com>
2024-05-19 19:23:55 +0530

    .

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,27 @@
-
+import React, { useEffect, useState } from 'react';
+import BannerHome from '../components/BannerHome.js';
+import { useSelector } from 'react-redux';
+import Card from '../components/Card.js';
+import HorizontalScollCard from '../components/HorizontalScrollCard.js';
+import axios from 'axios';
+import useFetch from '../hooks/';
 
 const Home = () => {
+
+  const trendingData = useSelector(state => state.movieoData.bannerData);
+  const { data : nowPlayingData } = useFetch('/movie/now_playing');
+  const { data : topRatedData } = useFetch('/movie/top_rated');
+  const { data : popularTvShowData } = useFetch('/tv/popular');
+  const { data : onTheAirShowDate } = useFetch('/tv/on_the_air');
+
   return (
     <div>
        <BannerHome/>
+       <HorizontalScollCard data={trendingData} heading={"Trending"} trending={true} />
+       <HorizontalScollCard data={nowPlayingData} heading={"Now Playing"} media_type={"movie"} />
+       <HorizontalScollCard data={topRatedData} heading={"Top Rated Movies"} media_type={"movie"} />
+       <HorizontalScollCard data={popularTvShowData} heading={"Popular TV Show"} media_type={"tv"} />
+       <HorizontalScollCard data={onTheAirShowData} />
     </div>
   )
 }
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,8 +1,94 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Card from '../components/Card';
 
 const SearchPage = () => {
+
+  const location = useLocation();
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const navigate = useNavigate();
+
+
+  const query = location?.search?.slice(3);
+
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`search/multi`, {
+        params: {
+          query: location?.search?.slice(3),
+          page: page
+        }
+      })
+
+      setData((preve) => {
+        return [
+          ...preve,
+          ...response.data.results
+        ]
+      })
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
+
+  useEffect(() => {
+    if (query) {
+      setPage(1)
+      setData([])
+      fetchData()
+    }
+  }, [location?.search]);
+
+
+  const handleScroll = () => {
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+      setPage(preve => preve + 1)
+    }
+  }
+
+
+  useEffect(() => {
+    if (query) {
+      fetchData()
+    }
+  }, [page])
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div>SearchPage</div>
+    <div className='py-16'>
+
+      <div className='lg:hidden my-2 mx-1 sticky top-[70px] z-30'>
+        <input
+          type='text'
+          placeholder='Seach here...'
+          onChange={(e) => navigate(`/search?q=${e.target.value}`)}
+          value={query?.split("%20")?.join(" ")}
+          className='px-4 py-1 text-lg w-full bg-white rounded-full text-neutral-900'
+        />
+      </div>
+
+      <div>
+         <h3></h3>
+
+         <div>
+            {
+              data.map((searchData, index) => {
+                return (
+                  <Card data={searchData} key={searchData.id+"search"} media_type={searchData.media_type} />
+                )
+              })
+            }
+         </div>
+      </div>
+    </div>
   )
 }
 
